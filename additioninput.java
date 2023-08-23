a3f57770e5486d6a2bdc7d388836a2b144465486
Yash <devilyash14@gmail.com>
2023-08-23 18:16:27 +0530

    user

--- additioninput.java
+++ additioninput.java
@@ -1,3 +1,20 @@
+import java.util.*;
 public class additioninput {
+    public static void main(String[] args) 
+    {
+        int num1,num2 ;
+        double result;
+    
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First number:");
+        num1 = sc.nextInt();
+        System.out.println("Enter the First number:");
+        num2 = sc.nextInt();
+        result = num1 + num2;
+
+        System.out.println("the addition is:"+result);
+
+        sc.close();
+    }
     
 }
--- largestinarray.java
+++ largestinarray.java
@@ -1,29 +1,35 @@
+
 //half done code
 import java.util.Scanner;
-public class largestinarray {
-    public static void main(String[]args)
-    {
-        int n;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of element:");
-        n =sc.nextInt();
 
-        int arr[]= new int [n];
-        System.out.println("Enter the array element:");
-        for(int i=0;i<arr.length;i++)
-        {
-            arr[i]=sc.nextInt(); 
+public class largestinarray {
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        // Create an array to store the numbers
+        int[] array = new int[n];
+
+        // Populate the array with user input
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
         }
-        int large1 = arr[0];
-        int large2 = arr[1];
-        
-        if(large1<large2)
-        {
-            int temp;
-            temp = large1;
-            large1 = large2;
-            large2 = temp;
+
+        // Find the largest number in the array
+        int largest = array[0];
+        for (int i = 1; i < n; i++) {
+            if (array[i] > largest) {
+                largest = array[i];
+            }
         }
-        sc.close();
+
+        // Display the largest number
+        System.out.println("The largest number in the array is: " + largest);
+
+        scanner.close();
     }
-}   
+}
